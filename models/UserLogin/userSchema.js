655f2f1a09fe503280e61820382c4d10751f2900
TyagiTanish <tanish@prologictechnologies.in>
2023-11-18 16:32:10 +0530

    changes

--- models/UserLogin/userSchema.js
+++ models/UserLogin/userSchema.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-const userSchema =  mongoose.Schema(
-=======
 const encrypt = require("./pre/save");
 // const methods = require("./methods");
 
 const userSchema = new mongoose.Schema(
->>>>>>> aeb6b3d1b19cf5d32dac6415e4fb1546af61e230
   {
     Name: {
       type: String,
@@ -18,21 +14,15 @@ const userSchema = new mongoose.Schema(
     },
     Phone: {
       type: String,
-<<<<<<< HEAD
-=======
       required: true,
     },
     Password: {
       type: String,
->>>>>>> aeb6b3d1b19cf5d32dac6415e4fb1546af61e230
       required: true,
     },
   },
   { timestamps: true }
 );
-<<<<<<< HEAD
-=======
 userSchema.pre("save", encrypt);
->>>>>>> aeb6b3d1b19cf5d32dac6415e4fb1546af61e230
 const userDetails = mongoose.model("userdetails", userSchema);
 module.exports = userDetails;
