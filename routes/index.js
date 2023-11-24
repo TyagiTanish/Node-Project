ae05a75467f4b2954c80bcc5a90328bd1683aa8a
TusharAtrii <tushar@prologictechnologies.in>
2023-11-24 11:52:29 +0530

    node changes

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,18 @@ const authentication = require("./Authentication");
 const user = require("./User");
 const auth = require("../middlewares/auth");
 const get = require("./User/get");
-const getMember = require('./User/member/post/getUser')
-const username=require('./User/update/username')
-const registerMember = require('./User/member/post/post')
+const getMember = require("./User/member/post/getUser");
+const username = require("./User/update/username");
+const registerMember = require("./User/member/post/post");
 
-const password=require('./User/update/password')
+const password = require("./User/update/password");
 const extractParam = require("../middlewares/extractParams/extractParams");
-const storage = multer.diskStorage({
-    destination: function (req, File, cb) {
-      // console.log(__dirname);
-      let fileLocation = path.join("./Images");
-      // console.log(fileLocation);
-      cb(null, fileLocation);
-    },
-    filename: function (req, File, cb) {
-      let fileName = Date.now() + File.originalname;
-      cb(null, fileName);
-    },
-  });
-  const upload = multer({
-    storage: storage,
-  }).array('files',3);
+
 router.use("/auth", authentication);
 router.use("/register", user);
-router.get('/getUserData',extractParam("authToken"),auth,get);
-router.put('/username/:id',extractParam("id"), username);
-router.put('/password/:id',extractParam("id"), password)
-router.post('/getMember',getMember)
+router.get("/getUserData", extractParam("authToken"), auth, get);
+router.put("/username/:id", extractParam("id"), username);
+router.put("/password/:id", extractParam("id"), password);
+router.post("/getMember", getMember);
 // router.post('/registerMember',registerMember)
 module.exports = router;
