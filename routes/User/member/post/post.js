2f16fd828f2ef56ca4d1b00ee85d0ede58f793c0
TusharAtrii <tushar@prologictechnologies.in>
2024-01-04 17:16:32 +0530

    addHotelAmenities

--- routes/User/member/post/post.js
+++ routes/User/member/post/post.js
@@ -1,29 +1,29 @@
-const hotelDetails = require("../../../../models/Hotel/hotelSchema")
-const userDetails = require("../../../../models/UserLogin/userSchema")
+const hotelDetails = require("../../../../models/Hotel/hotelSchema");
+const userDetails = require("../../../../models/UserLogin/userSchema");
 
-module.exports = async (req,res) => {
-    const user = new userDetails({
-    name:req.body.name,
-    email:req.body.email,
-    phone:req.body.phone,
-    password:req.body.password,
-    role:'member',
+module.exports = async (req, res) => {
+  const user = new userDetails({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password,
+    role: "member",
+  });
+  user.save();
+  const data = new hotelDetails({
+    ownerId: user._id,
+    hotelName: req.body.name,
+    location: { latitude: req.body.latitude, longitude: req.body.longitude },
+    photo: req.files[0].path,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    pinCode: req.body.pinCode,
+    rooms: [],
+    amenities: req.body.amenities,
+  });
 
-})
-user.save();
-    const data = new hotelDetails({
-        ownerId:user._id,
-        hotelName:req.body.name,
-        location: {latitude:req.body.latitude,longitude:req.body.longitude},
-        photo:req.files[0].path,
-        city:req.body.city,
-        state:req.body.state,
-        country:req.body.country,
-        pinCode:req.body.pinCode,
-        rooms:[],
-    })
-
-    console.log(data);
-data.save();
-    res.json({test: true})
-}
+  console.log(data);
+  data.save();
+  res.json({ test: true });
+};
