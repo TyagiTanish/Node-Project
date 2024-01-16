af888c90c705e829a4bcac389bd65ed1b025c5d4
Navjot <navjot@prologictechnologies.in>
2024-01-16 11:15:14 +0530

    changes in n node

--- routes/User/update/username.js
+++ routes/User/update/username.js
@@ -1,7 +1,7 @@
 const userSchema = require("../../../models/UserLogin/userSchema");
 
 const username = async (req, res) => {
-  const id = req.id;
+  const  id = req.id;
   const name = req.body.name;
   const phone = req.body.phone;
   const data = await userSchema.findByIdAndUpdate(id, {
