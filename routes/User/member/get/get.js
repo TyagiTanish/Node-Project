ebb8ee7d3b40c64ccba7d5be9668af69d143adea
Navjot <navjot@prologictechnologies.in>
2024-01-16 12:00:31 +0530

    changes in Hotel

--- routes/User/member/get/get.js
+++ routes/User/member/get/get.js
@@ -1,9 +1,17 @@
 const hotelDetails = require('../../../../models/Hotel/hotelSchema');
-const userDetails=require('../../../../models/Hotel/hotelSchema');
+const userSchema=require('../../../../models/UserLogin/userSchema');
+const { findById, findByIdAndUpdate } = require('../../../../models/UserLogin/userSchema');
 module.exports=async(req,res)=>{
     const user = req.user;
 //    const data= await hotelDetails.findOne({email:user.email})
     const data=await hotelDetails.find({ownerId:user._id});
-   
+   if(data.length===0)
+   {
+    const result = await userSchema.findByIdAndUpdate(user._id, {
+        role: 'customer',
+      });
+         
+   }
+
     res.send(data)
 }
