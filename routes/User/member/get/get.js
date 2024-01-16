6d4ccd547a63e712b980475630ffff0d09db3fe1
Navjot <navjot@prologictechnologies.in>
2024-01-16 12:02:38 +0530

    new changes

--- routes/User/member/get/get.js
+++ routes/User/member/get/get.js
@@ -3,7 +3,6 @@ const userSchema=require('../../../../models/UserLogin/userSchema');
 const { findById, findByIdAndUpdate } = require('../../../../models/UserLogin/userSchema');
 module.exports=async(req,res)=>{
     const user = req.user;
-//    const data= await hotelDetails.findOne({email:user.email})
     const data=await hotelDetails.find({ownerId:user._id});
    if(data.length===0)
    {
