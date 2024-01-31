58ffd047a170978f0f4831333af383802b1d1be1
Navjot <navjot@prologictechnologies.in>
2024-01-31 12:55:49 +0530

    changes in nodejs

--- routes/User/member/bookingDetails.js
+++ routes/User/member/bookingDetails.js
@@ -1,13 +1,62 @@
 const bookings = require('../../../models/Billing/bookingSchema');
 
-module.exports=async(req,res)=>{
-    const user=req.user;
-    const result=await bookings.find({ownerId:user._id}).populate("hotelId");
 
-    const data=result.filter((item)=>{
-        return item.hotelId!=null
-    })
-    console.log('data is .............',data)
-    res.send(data)
+ /**
+ * @method GET
+ * @api /bookingDetails
+ * @description
+ *  - To get booking Details that are pending 
+ */
+module.exports=async(req,res)=>{
+    if(Object.keys(req.query).length === 0){
+        const user=req.user;
+        const result=await bookings.find({ownerId:user._id,status:"pending"}).populate("hotelId");
+    
+        const data=result.filter((item)=>{
+            return item.hotelId!=null
+        })
+      
+        res.send(data);
+    }
+    else{
+        const limit = req.query.limit;
+        const page = req.query.page;
+        const user=req.user;
+        const sortby = req.query.sortby === "asc" ? 1 : -1;
+        const orderby = req.query.orderby;
+        const search=req.query.search;
+        if(search===""){
+            const result=await bookings.find({ownerId:user._id,status:"pending"}).limit(limit)
+            .skip(limit * page)
+            .sort({ [orderby]: sortby })
+            .populate("hotelId");
+            const data=result.filter((item)=>{
+                return item.hotelId!=null
+            })
+          
+            res.send(data)
+        }
+      
+       else{
+        const result=await bookings.find({ownerId:user._id,status:"pending", $or: [
+            { fullName: { $regex: search, $options: 'i' } },
+            { email: { $regex: search, $options: "i" } },
+            { phone: { $regex: search, $options: "i" } },
+    
+            // { bookTo: { $regex: search, $options: "i" } },
+            
+          ]}).limit(limit)
+        .skip(limit * page)
+        .sort({ [orderby]: sortby })
+        
+        .populate("hotelId");
+        const data=result.filter((item)=>{
+            return item.hotelId!=null
+        })
+      
+        res.send(data)
+       }
+    }
+   
     
 }        
