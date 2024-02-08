f9144e7e6d8d240eab2834a13c44635eeb94cb4e
TanishTyagi <tanish@prologictechnologies.in>
2024-02-08 10:08:37 +0530

    changes for html-pdf

--- routes/payment/html-pdf-convert.js
+++ routes/payment/html-pdf-convert.js
@@ -1,18 +1,25 @@
-var htmlPdf = require('html-pdf');
+var htmlPdf = require("html-pdf");
 
-let options = { format: 'letter',type:'pdf'};
-module.exports = async (template,key)=>{
-    let file = template;
-    const buffer = new Promise((resolve, reject) => {
-        htmlPdf.create(file, options).toBuffer(function(err, res) {
-            if (err) { 
-                console.log({err})
-                reject(err);
-            }
-            else{
-                resolve(res);
-            }
-          });
-    })
-    return buffer
-}
+let options = {
+  type: "pdf",
+  format: "Letter",
+  childProcessOptions: {
+    env: {
+      OPENSSL_CONF: "/dev/null",
+    },
+  },
+};
+module.exports = async (template, key) => {
+  let file = template;
+  const buffer = new Promise((resolve, reject) => {
+    htmlPdf.create(file, options).toBuffer(function (err, res) {
+      if (err) {
+        console.log({ err });
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+  return buffer;
+};
