d944c262d3e4f61a17ef761c296fa82dc9191173
Tushar Atrii <tushar@prologoictechnologies.in>
2024-03-21 18:39:27 +0530

    make API for updating payment status

--- routes/User/member/acceptedBookings.js
+++ routes/User/member/acceptedBookings.js
@@ -1,65 +1,74 @@
-const bookings = require('../../../models/Billing/bookingSchema');
+const bookings = require("../../../models/Billing/bookingSchema");
 
 module.exports = async (req, res) => {
-    try {
+  try {
+    if (Object.keys(req.query).length === 0) {
+      const user = req.user;
 
-        if (Object.keys(req.query).length === 0) {
-            const user = req.user;
+      const result = await bookings
+        .find({ ownerId: user._id, status: { $ne: "pending" } })
+        .populate("ownerId")
+        .populate("hotelId")
+        .populate("roomId")
+        .populate("paymentId");
 
-            const result = await bookings.find({ ownerId: user._id }).populate('ownerId').populate('hotelId').populate("roomId");
+      res.send(result);
+    } else {
+      const limit = req.query.limit;
+      const page = req.query.page;
+      const sortby = req.query.sortby === "asc" ? 1 : -1;
+      const orderby = req.query.orderby;
+      const search = req.query.search;
 
+      const user = req.user;
 
-            res.send(result)
-        }
-        else {
+      if (search === "") {
+        const result = await bookings
+          .find({
+            ownerId: user._id,
+            status: { $ne: "pending" },
+          })
 
-            const limit = req.query.limit;
-            const page = req.query.page;
-            const sortby = req.query.sortby === "asc" ? 1 : -1;
-            const orderby = req.query.orderby;
-            const search = req.query.search;
+          .limit(limit)
+          .skip(limit * page)
+          .sort({ [orderby]: sortby })
+          .populate("ownerId")
+          .populate("hotelId")
+          .populate("roomId")
+          .populate("paymentId");
+        res.send(result);
+      } else {
+        const result = await bookings
+          .find({
+            ownerId: user._id,
+            $or: [
+              { "hotelId.hotelName": { $regex: search, $options: "i" } },
+              { email: { $regex: search, $options: "i" } },
+              { bookFrom: { $regex: search, $options: "i" } },
 
-            const user = req.user;
-
-            if (search === "") {
-                const result = await bookings.find({ ownerId: user._id })
-                    .limit(limit)
-                    .skip(limit * page)
-                    .sort({ [orderby]: sortby })
-                    .populate('ownerId').populate('hotelId').populate("roomId");
-                res.send(result);
-            }
-
-            else {
-                const result = await bookings.find({
-                    ownerId: user._id, $or: [
-                        { 'hotelId.hotelName': { $regex: search, $options: 'i' } },
-                        { email: { $regex: search, $options: "i" } },
-                        { bookFrom: { $regex: search, $options: "i" } },
-
-                        { bookTo: { $regex: search, $options: "i" } },
-                        { paymentStatus: { $regex: search, $options: "i" } },
-                        { status: { $regex: search, $options: "i" } },
-                    ],
-                })
-                    .limit(limit)
-                    .skip(limit * page)
-                    .sort({ [orderby]: sortby })
-                    .populate('ownerId').populate('hotelId').populate("roomId");
-                res.send(result)
-            }
-
-
-        }
-
+              { bookTo: { $regex: search, $options: "i" } },
+              { paymentStatus: { $regex: search, $options: "i" } },
+              { status: { $regex: search, $options: "i" } },
+            ],
+            status: { $ne: "pending" },
+          })
+          .limit(limit)
+          .skip(limit * page)
+          .sort({ [orderby]: sortby })
+          .populate("ownerId")
+          .populate("hotelId")
+          .populate("roomId")
+          .populate("paymentId");
+        res.send(result);
+      }
     }
-    catch (error) {
-        res.send(error)
-    }
-}
+  } catch (error) {
+    res.send(error);
+  }
+};
 /**
-* @method GET
-* @api /acceptedBookings
-* @description
-*  - To get booking Details that are accepted by the Owner
-*/
+ * @method GET
+ * @api /acceptedBookings
+ * @description
+ *  - To get booking Details that are accepted by the Owner
+ */
