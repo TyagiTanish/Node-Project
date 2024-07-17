2cca977c36056a79baf6dc0739362adef756f5c1
Tanish tyagi <tanish@prologictechnologies.in>
2024-07-17 12:19:32 +0530

    changes

--- routes/payment/booking.js
+++ routes/payment/booking.js
@@ -2,11 +2,11 @@
 const { log } = require("console");
 const express = require("express");
 const stripe = require("stripe")(
-  "sk_test_51Li8s7BQTREDaPxY7AUklpQjHmx29Nzcxquawr3KJEHWy3i4TQiVrftMnH0zLTgwnt9t1hVZrhN64OI10GrG39Fc00cR4utdeX"
+  process.env.StripeSecret
 );
 module.exports = async (req, res) => {
   try {
-    const  {token} = JSON.parse(req.body.body.token)
+    const { token } = JSON.parse(req.body.body.token)
     const price = req.body.body.price
     const charge = await stripe.charges.create({
       amount: req.body.body.price, // amount in cents
