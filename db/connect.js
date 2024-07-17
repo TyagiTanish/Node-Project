72166bd76c2320494e148363e752aea746fd2603
TanishTyagi <151004296+TyagiTanish@users.noreply.github.com>
2024-07-17 12:54:41 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const connect = async () => {
   // conn = await mongoose.connect("mongodb://localhost:27017", {
   //   dbName: "OyoProject",
   // });
-  conn = await mongoose.connect("mongodb://admin:JKuighk97%21%40klnjgv@192.168.1.190:27017/?retryWrites=true&loadBalanced=false&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",{
+  conn = await mongoose.connect("mongodb+srv://priyanshu05:CdIedyYoGee1y0MQ@cluster0.uzhu2.mongodb.net/",{
     dbName:'OyoProject'
   });
   if (conn) {
